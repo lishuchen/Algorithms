25c5f7c04379b88e3397533356e724287b616fc3
lishuchen <andrewdebug@gmail.com>
2017-06-02 23:25:28 -0700

    20170602 - Correct 239

--- leecode/239_Sliding_Window_Maximum.py
+++ leecode/239_Sliding_Window_Maximum.py
@@ -15,11 +15,11 @@ class Solution(object):
         dq = deque()
         rsl = []
         for i in range(len(nums)):
+            while dq and dq[0] < i - k + 1:
+                dq.popleft()
             while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
             dq.append(i)
-            if i - k == dq[0]:
-                dq.popleft()
             if i >= k - 1:
                 rsl.append(nums[dq[0]])
         return rsl
